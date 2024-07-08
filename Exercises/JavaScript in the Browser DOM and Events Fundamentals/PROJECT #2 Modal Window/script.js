7c71820d218b759af56b10a749cadf2bc3acd047
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-08 18:20:37 +0200

    PROJECT #2: Modal Window

--- Exercises/JavaScript in the Browser DOM and Events Fundamentals/PROJECT #2 Modal Window/script.js
+++ Exercises/JavaScript in the Browser DOM and Events Fundamentals/PROJECT #2 Modal Window/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const showModal = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+
+for (let i of showModal) {
+  i.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+function addHidden() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+closeModal.addEventListener('click', function () {
+  addHidden();
+});
+
+overlay.addEventListener('click', function () {
+  addHidden();
+});
