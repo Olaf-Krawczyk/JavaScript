490d94cd8d82abf0bb4516f62500bfae57248ef8
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-09 17:50:29 +0200

    PROJECT #3: Pig Game

--- Exercises/JavaScript in the Browser DOM and Events Fundamentals/PROJECT #3 Pig Game/script.js
+++ Exercises/JavaScript in the Browser DOM and Events Fundamentals/PROJECT #3 Pig Game/script.js
@@ -3,7 +3,32 @@
 const score0 = document.querySelector('#score--0');
 const score1 = document.getElementById('score--1');
 const dice = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const currentScore = document.querySelector('.current-score');
 
 dice.classList.add('hidden');
-const hidden = document.querySelector('.hidden');
-hidden.style.display = 'none';
+
+const scores = [0, 0];
+let playerScore = 0;
+let activePlayer = 0;
+
+btnRoll.addEventListener('click', function () {
+  const random = Math.trunc(Math.random() * 6 + 1);
+  dice.classList.remove('hidden');
+  dice.setAttribute('src', `dice-${random}.png`);
+
+  if (activePlayer === 0) {
+    if (random !== 1) {
+      playerScore += random;
+      currentScore.textContent = playerScore;
+    } else {
+      activePlayer = 1;
+    }
+  } else if (activePlayer === 1)
+    if (random !== 1) {
+      playerScore += random;
+      currentScore.textContent = playerScore;
+    } else {
+      activePlayer = 0;
+    }
+});
