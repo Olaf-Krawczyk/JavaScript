3baf507b4045e1927c51a2e5cdbf267a7c213fb3
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-09 19:09:54 +0200

    PROJECT #3: Pig Game

--- Exercises/JavaScript in the Browser DOM and Events Fundamentals/PROJECT #3 Pig Game/script.js
+++ Exercises/JavaScript in the Browser DOM and Events Fundamentals/PROJECT #3 Pig Game/script.js
@@ -4,7 +4,14 @@ const score0 = document.querySelector('#score--0');
 const score1 = document.getElementById('score--1');
 const dice = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
-const currentScore = document.querySelector('.current-score');
+const currentScore0 = document.querySelector('#current--0');
+const currentScore1 = document.querySelector('#current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+const btnHold = document.querySelector('.btn--hold');
+const scoreOvr0 = document.querySelector('#score--0');
+const scoreOvr1 = document.querySelector('#score--1');
+const btnNew = document.querySelector('.btn--new');
 
 dice.classList.add('hidden');
 
@@ -20,15 +27,43 @@ btnRoll.addEventListener('click', function () {
   if (activePlayer === 0) {
     if (random !== 1) {
       playerScore += random;
-      currentScore.textContent = playerScore;
+      currentScore0.textContent = playerScore;
     } else {
       activePlayer = 1;
+      playerScore = 0;
+      currentScore0.textContent = playerScore;
+      player0.classList.remove('player--active');
+      player1.classList.add('player--active');
     }
   } else if (activePlayer === 1)
     if (random !== 1) {
       playerScore += random;
-      currentScore.textContent = playerScore;
+      currentScore1.textContent = playerScore;
     } else {
       activePlayer = 0;
+      playerScore = 0;
+      currentScore1.textContent = playerScore;
+      player1.classList.remove('player--active');
+      player0.classList.add('player--active');
     }
+});
+
+btnHold.addEventListener('click', function () {
+  if (activePlayer === 0) {
+    scores[activePlayer] += playerScore;
+    scoreOvr0.textContent = scores[activePlayer];
+    player0.classList.remove('player--active');
+    player1.classList.add('player--active');
+    activePlayer = 1;
+    playerScore = 0;
+    currentScore0.textContent = 0;
+  } else if (activePlayer === 1) {
+    scores[activePlayer] += playerScore;
+    scoreOvr1.textContent = scores[activePlayer];
+    player1.classList.remove('player--active');
+    player0.classList.add('player--active');
+    playerScore = 0;
+    currentScore1.textContent = 0;
+    activePlayer = 0;
+  }
 });
