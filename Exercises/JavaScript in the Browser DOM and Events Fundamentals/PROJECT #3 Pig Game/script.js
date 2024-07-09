1a4e45c8417d76e8467f96b5e7c662a579e9eb2d
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-09 13:46:40 +0200

    PROJECT #3: Pig Game

---
+++ Exercises/JavaScript in the Browser DOM and Events Fundamentals/PROJECT #3 Pig Game/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const score0 = document.querySelector('#score--0');
+const score1 = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+dice.classList.add('hidden');
+const hidden = document.querySelector('.hidden');
+hidden.style.display = 'none';
