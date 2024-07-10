79e9146a62b4d6e432a7f1526b0adf374c9bc1f7
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-10 16:14:19 +0200

    Coding Challenge #2

--- Exercises/Data Structures, Modern Operators and Strings/Coding Challenge #2/main.js
+++ Exercises/Data Structures, Modern Operators and Strings/Coding Challenge #2/main.js
@@ -14,3 +14,26 @@ const game = {
     team2: 6.5,
   },
 };
+
+for (let playerName of game.scored) {
+  console.log(`${playerName} scored ${1} goal`);
+}
+
+const odd = Object.values(game.odds);
+
+let sum = 0;
+for (let y of odd) {
+  sum += y;
+}
+
+const odd2 = Object.entries(game.odds);
+
+for (let [i, k] of odd2) {
+  if (i === "team1") {
+    console.log(`team 1 will win: ${k}`);
+  } else if (i === "team2") {
+    console.log(`team 2 will win: ${k}`);
+  } else {
+    console.log(`draw: ${k}`);
+  }
+}
