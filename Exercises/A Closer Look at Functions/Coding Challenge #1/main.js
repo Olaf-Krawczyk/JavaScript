a033c89ab45fe538f721de6801fa05623130b604
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-12 19:56:16 +0200

    Coding Challenge #1

---
+++ Exercises/A Closer Look at Functions/Coding Challenge #1/main.js
@@ -0,0 +1,21 @@
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const userOption = prompt("Whats your favourite programming language");
+    if (userOption == 0) {
+      this.answers[0] += 1;
+    } else if (userOption == 1) {
+      this.answers[1] += 1;
+    } else if (userOption == 2) {
+      this.answers[2] += 1;
+    } else if (userOption == 3) {
+      this.answers[3] += 1;
+    }
+  },
+};
+
+poll.registerNewAnswer();
+
+console.log(poll);
