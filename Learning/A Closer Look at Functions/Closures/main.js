f8af8be2cf940bcd250289a187b7df364114c24d
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-12 20:36:06 +0200

    Closures

--- Learning/A Closer Look at Functions/Closures/main.js
+++ Learning/A Closer Look at Functions/Closures/main.js
@@ -7,3 +7,26 @@ const secureBooking = function () {
 };
 
 const booker = secureBooking();
+
+let f;
+
+const q = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+q();
+
+f();
+
+h();
+f();
