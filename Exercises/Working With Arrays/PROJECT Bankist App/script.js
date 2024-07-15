1b22e0e33d18e3faac4cf5d0f3fdcf3ce3c09305
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-15 16:39:28 +0200

    PROJECT Bankist App

--- Exercises/Working With Arrays/PROJECT Bankist App/script.js
+++ Exercises/Working With Arrays/PROJECT Bankist App/script.js
@@ -151,3 +151,17 @@ btnTransfer.addEventListener('click', function (e) {
     }
   }
 });
+
+btnClose.addEventListener('click', function (x) {
+  x.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    inputClosePin.value == currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      x => x.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+});
