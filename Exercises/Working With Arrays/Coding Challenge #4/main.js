74c652c64e29da1ce4a72844cd2564f50c4f5961
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-16 15:12:33 +0200

    Coding Challenge #4

---
+++ Exercises/Working With Arrays/Coding Challenge #4/main.js
@@ -0,0 +1,14 @@
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
+
+const recFood = dogs.forEach((element) => {
+  console.log(Math.trunc(element.weight ** 0.75 * 28 * 1000));
+});
+
+const index = dogs.flatMap((x) => x.owners).findIndex((x) => x === "Sarah");
+
+console.log(sarahDog);
