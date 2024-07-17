b110d7c322e0c7f0619d8a71df4e3f691fff3302
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-17 19:08:44 +0200

    Math and Rounding

---
+++ Learning/Numbers, Dates, Intl and Timers/Math and Rounding/main.js
@@ -0,0 +1,20 @@
+console.log(Math.sqrt(25));
+
+console.log(Math.max(5, 10, 15, 20, 18, 25));
+
+console.log(Math.min(5, 10, 15, 20, 18, 25));
+
+console.log(Math.trunc(Math.random() * 6 + 1));
+
+const minMax = (min, max) => Math.trunc(Math.random() * (1 + max - min) + 1);
+
+console.log(minMax(1, 10));
+
+console.log(Math.round(23.2));
+
+console.log(Math.round(23.6));
+
+console.log(Math.ceil(23.6));
+console.log(Math.floor(23.6));
+
+console.log((2.52).toFixed(3));
