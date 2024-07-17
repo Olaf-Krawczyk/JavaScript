87a82fcd9c3882d3f058a2c22389adccbd604457
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-17 18:56:04 +0200

    Converting and Checking Numbers

---
+++ Learning/Numbers, Dates, Intl and Timers/Converting and Checking Numbers/main.js
@@ -0,0 +1,15 @@
+console.log(23 === 23.0);
+
+console.log(0.1 + 0.2);
+
+console.log(0.1 + 0.2 === 3);
+
+console.log(Number("23"));
+
+console.log(+"23");
+
+console.log(parseInt(`23px`));
+
+console.log(Number.isNaN(`w`));
+
+console.log(Number.isFinite(20.0));
