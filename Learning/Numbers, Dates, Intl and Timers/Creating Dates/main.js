5677dc9233f80c8391b3d80a2132e75ffba95a76
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-18 15:11:38 +0200

    Creating Dates

---
+++ Learning/Numbers, Dates, Intl and Timers/Creating Dates/main.js
@@ -0,0 +1,32 @@
+// const now = new Date();
+
+// console.log(now);
+
+// console.log(new Date(`December 24, 2015`));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+
+console.log(future.getFullYear());
+
+console.log(future.getMonth());
+
+console.log(future.getDate());
+
+console.log(future.getDay());
+
+console.log(future.getHours());
+
+console.log(future.getMinutes());
+
+console.log(future.getSeconds());
+
+console.log(future.toISOString());
+
+console.log(future.getTime());
+
+console.log(new Date(2142253380000));
+
+console.log(Date.now());
