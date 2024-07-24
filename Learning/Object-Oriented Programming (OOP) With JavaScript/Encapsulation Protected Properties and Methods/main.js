725514cee4c675702f438e20fb61cf63022c3bbc
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-24 20:26:10 +0200

    Encapsulation: Protected Properties and Methods

---
+++ Learning/Object-Oriented Programming (OOP) With JavaScript/Encapsulation Protected Properties and Methods/main.js
@@ -0,0 +1,40 @@
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this._pin = pin;
+    this._movements = [];
+    this.locale = navigator.language;
+  }
+
+  getMovements() {
+    console.log(this._movements);
+  }
+
+  deposit(value) {
+    this._movements.push(value);
+  }
+
+  withdrawal(value) {
+    this.deposit(-value);
+  }
+
+  approvedLoan() {
+    return true;
+  }
+
+  reqLoan(value) {
+    if (this.approvedLoan(value)) {
+      this.deposit(value);
+      console.log("APPROVED");
+    }
+  }
+}
+
+const accoun1 = new Account("Mark", "EUR", 1111);
+
+accoun1.deposit(250);
+
+accoun1.withdrawal(250);
+
+accoun1.reqLoan(250);
