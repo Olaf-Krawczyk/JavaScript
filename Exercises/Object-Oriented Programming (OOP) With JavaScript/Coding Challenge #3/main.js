42581515091a1b18054413f05f87fb41f763cce6
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-24 10:46:52 +0200

    Coding Challenge #3

---
+++ Exercises/Object-Oriented Programming (OOP) With JavaScript/Coding Challenge #3/main.js
@@ -0,0 +1,27 @@
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(`${this.speed} at ${this.charge}`);
+};
+
+const bmw = new EV("BMW", 120, 23);
+
+bmw.chargeBattery(90);
+
+bmw.accelerate();
