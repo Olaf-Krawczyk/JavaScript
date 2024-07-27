770450f0d1c6930926c740b0e2f272bed95ff6a6
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-27 17:52:50 +0200

    PROJECT Mapty

--- Learning/Mapty App OOP, Geolocation, External Libraries, and More!/Mapty/script.js
+++ Learning/Mapty App OOP, Geolocation, External Libraries, and More!/Mapty/script.js
@@ -82,67 +82,79 @@ class App {
   }
 
   _getPosition() {
+    // pobieramy pozycje uzytkownika
     if (navigator.geolocation) {
+      // jesli pozycja moze zostac pobrana
       navigator.geolocation.getCurrentPosition(
-        this._loadMap.bind(this),
+        // to pytamy sie o pobranie
+        this._loadMap.bind(this), // uzywamy funckji ktora zaladuje mape
         function () {
-          alert('Could not get your position');
+          alert('Could not get your position'); // jesliu uzytkownik nie wyrazi zgody to pojawi sie alert
         }
       );
     }
   }
 
   _loadMap(position) {
-    const { latitude } = position.coords;
+    // funckja ktora laduje mape
+    const { latitude } = position.coords; // kordy
     const { longitude } = position.coords;
 
-    const coords = [latitude, longitude];
+    const coords = [latitude, longitude]; // przypisujemy jedne i drugie kordy do tabeli
 
-    this.#map = L.map('map').setView(coords, this.#mapzoomlevel);
+    this.#map = L.map('map').setView(coords, this.#mapzoomlevel); //naszemu #map dajemy mape
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      // zmieniamy styyl
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.#map);
+    }).addTo(this.#map); // dodajemy wizje dla naszej mapy
 
-    this.#map.on('click', this._showForm.bind(this));
+    this.#map.on('click', this._showForm.bind(this)); // kiedy klikniemy na mape to wykona sie evenet ktory pokaze nam forma
 
     this.#workouts.forEach(element => {
-      this._renderWorkoutMaker(element);
+      // petla dla kazdego rzeczy w #workouts
+      this._renderWorkoutMaker(element); // dla kazdego elementu renderujemy marker
     });
   }
 
   _showForm(mapE) {
-    this.#mapEvent = mapE;
-    form.classList.remove('hidden');
-    inputDistance.focus();
+    // funckja show form ktora pokaze forma
+
+    // mapE kazde nasze kliknieceie
+    this.#mapEvent = mapE; // przypisujemy kazde nasze kliknieceie do mapEvent
+    form.classList.remove('hidden'); // sciagamy klase hidden
+    inputDistance.focus(); // ustawiamy focus na inputDistance
   }
 
   _hideForm() {
     inputElevation.value = '';
     inputCadence.value = '';
     inputDuration.value = '';
-    inputDistance.value = '';
-    form.style.display = 'none';
-    form.classList.add('hidden');
+    inputDistance.value = ''; // reset wszystkich pól
+    form.style.display = 'none'; // zmieniamy na display none
+    form.classList.add('hidden'); //dodajemy klase hidden
 
     setTimeout(() => {
+      // uzywamnt triku zeby nasze elementu podmniely sie bez animacji
       form.style.display = 'grid';
     }, 1000);
   }
 
   _toggleElevationsField() {
+    // funkcja ktora zmienia nam element w zalezonosci od wybranej lubryki
     inputDistance.focus();
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden'); //toggle wylacza albo wlacza dana opcje
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden'); //wiecj jesli jest odpalona to ją zdjemie albo na odwrot
   }
   _newWorkout(e) {
+    // tworzy nowy workout
     function validInputs(...inputs) {
       return inputs.every(inp => Number.isFinite(inp)); //sprawdzanie czy wszystki liczby sa pozytywne a nie np sa literami
     }
 
     function allPositive(...inputs) {
-      return inputs.every(inp => inp > 0);
+      return inputs.every(inp => inp > 0); // sprawdza czy liczby sa wieksze niz 0
     }
 
     e.preventDefault();
@@ -150,8 +162,8 @@ class App {
     const type = inputType.value;
     const distance = +inputDistance.value;
     const duration = +inputDuration.value;
-    const { lat, lng } = this.#mapEvent.latlng;
-    let workout;
+    const { lat, lng } = this.#mapEvent.latlng; // wyciagamy z #mapEvent nasze coordy
+    let workout; // tworzymy workout
 
     if (type === 'running') {
       const cadence = +inputCadence.value;
@@ -161,7 +173,7 @@ class App {
       )
         return alert('ABVC');
 
-      workout = new Running([lat, lng], distance, duration, cadence);
+      workout = new Running([lat, lng], distance, duration, cadence); // jesli wszystko bedzie git to stworzymy nowy Running i przypisze go do workout
     }
 
     if (type === 'cycling') {
@@ -175,34 +187,36 @@ class App {
       workout = new Cycling([lat, lng], distance, duration, elevation);
     }
 
-    this.#workouts.push(workout);
+    this.#workouts.push(workout); // pozniej do tabeli workouts damy workout
 
-    this._renderWorkoutMaker(workout);
-    this._renderWorkout(workout);
-    this._hideForm();
+    this._renderWorkoutMaker(workout); // funckja renderujaca marker
+    this._renderWorkout(workout); // funckja renderujaca workout
+    this._hideForm(); // funckja chowajaca forma
 
-    this._setLocalStorage();
+    this._setLocalStorage(); // funckja robiaca local storage
   }
 
-  _renderWorkoutMaker(workout, type) {
-    L.marker(workout.coords)
-      .addTo(this.#map)
+  _renderWorkoutMaker(workout) {
+    // tworzy marker
+    L.marker(workout.coords) // na coordach z workout
+      .addTo(this.#map) //dodajmy do naszej mapy
       .bindPopup(
         L.popup({
           maxWidth: 250,
           minWidth: 120,
           autoClose: false,
           closeOnClick: false,
-          className: `${workout.type}-popup`,
+          className: `${workout.type}-popup`, // i tworzy nam na podstawie type
         })
       )
       .setPopupContent(
-        `${workout.type === 'running' ? `🏃‍♂️` : `🚴‍♀️`} ${workout.description}`
+        `${workout.type === 'running' ? `🏃‍♂️` : `🚴‍♀️`} ${workout.description}` // kontent pobieramy z workout description
       )
       .openPopup();
   }
 
   _renderWorkout(workout) {
+    //tworzyny workouty z pomoca html
     let html = `<li class="workout workout--${workout.type}" data-id="${
       workout.id
     }">
@@ -249,37 +263,40 @@ class App {
         </li>`;
     }
 
-    form.insertAdjacentHTML('afterend', html);
+    form.insertAdjacentHTML('afterend', html); // dodajemy do html
   }
 
   _moveToPopup(e) {
-    const workoutEl = e.target.closest('.workout');
+    // tam gdzue klikniemy tam nas przeniesie
+    const workoutEl = e.target.closest('.workout'); // wyszuka najblizsza klase .workout
     if (!workoutEl) return;
 
-    const workout = this.#workouts.find(x => x.id === workoutEl.dataset.id);
+    const workout = this.#workouts.find(x => x.id === workoutEl.dataset.id); // ze wszystkich workout znajdzie ten z odpowiednim datasetem
     this.#map.setView(workout.coords, this.#mapzoomlevel, {
+      // funckja ktora ustawi nam z animacja
       animate: true,
       pan: { duration: 1 },
     });
   }
 
   _setLocalStorage() {
-    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts)); //setujemy nasz local storage i zmieniamy go w stringa
   }
   _getLocalStorage() {
-    const data = JSON.parse(localStorage.getItem('workouts'));
+    const data = JSON.parse(localStorage.getItem('workouts')); //pobieramy nasz local storage i zmieniamy go ze stringa w object
 
-    if (!data) return;
-    this.#workouts = data;
+    if (!data) return; // jesli nie ma daty to nic sie nie wydarzy
+    this.#workouts = data; // przypisujemy date z local stroge do nasszych workouts
     this.#workouts.forEach(element => {
-      this._renderWorkout(element);
+      this._renderWorkout(element); //dla kazdego elementu renderujemy workout po lewej
     });
   }
 
   reset() {
+    // resetujemy nasz local storage
     localStorage.removeItem('workouts');
     location.reload();
   }
 }
 
-const app = new App();
+const app = new App(); // tworzymy nowa App
