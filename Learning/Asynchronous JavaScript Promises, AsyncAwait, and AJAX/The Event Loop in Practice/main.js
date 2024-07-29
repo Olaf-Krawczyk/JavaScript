566aa021131e472ae5eb33ceb0a33c1c9d538a8b
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-07-29 19:36:23 +0200

    The Event Loop in Practice

--- Learning/Asynchronous JavaScript Promises, AsyncAwait, and AJAX/The Event Loop in Practice/main.js
+++ Learning/Asynchronous JavaScript Promises, AsyncAwait, and AJAX/The Event Loop in Practice/main.js
@@ -2,4 +2,10 @@ console.log("TEST START");
 
 setTimeout(() => console.log(`object`), 0);
 Promise.resolve(`Resolved promise 1`).then((response) => console.log(response));
+
+Promise.resolve("resolved proimse 2").then((response) => {
+  for (let i = 0; i < 2000000; i++) {}
+  console.log(response);
+});
+
 console.log(`TEST END`);
