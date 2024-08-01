b0dd88b7ecdd2b630ae1e25e80fde5c1549a1f37
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-01 19:38:23 +0200

    Exporting and Importing in ES6 Modules

---
+++ Learning/Modern JavaScript Development Modules, Tooling, and Functional/Exporting and Importing in ES6 Modules/main.js
@@ -0,0 +1,10 @@
+// import { addToCart, c } from "./shoppingCart.js";
+
+// addToCart(`bread`, 5);
+// addToCart(`bread`, 5);
+
+// console.log(c);
+
+import * as ShoppingCart from "./shoppingCart.js";
+
+console.log(ShoppingCart);
---
+++ Learning/Modern JavaScript Development Modules, Tooling, and Functional/Exporting and Importing in ES6 Modules/shoppingCart.js
@@ -0,0 +1,11 @@
+console.log("EXPORTt");
+
+const shippingCost = 10;
+const cart = [];
+
+export function addToCart(product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`YOU ADD`);
+}
+
+export { cart as c };
