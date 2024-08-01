198b3faf178f044856cdf93030fce66471487533
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-01 20:07:38 +0200

    CommonJS Modules

---
+++ Learning/Modern JavaScript Development Modules, Tooling, and Functional/CommonJS Modules/main.js
@@ -0,0 +1,10 @@
+const cart = [];
+
+function addToCart(item, qty) {
+  cart.push({ item, qty });
+  console.log(`YOU ADDED ${item} x${qty}`);
+}
+
+addToCart(`bread`, 5);
+
+const { addToCart } = require("");
